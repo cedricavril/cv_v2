5b13585108de46220ac6c4a21161c66041711c3e
Cédric <cedric_avril_pro@hotmail.fr>
2022-08-08 21:44:32 +0200

    commiting the files

---
+++ integrateur_html_css.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Curriculum vitae intégrateur HTML/CSS</title>
+  <?php require("include/head.html") ?>
+  <link rel="stylesheet" type="text/css" href="style_integrateur.css">
+</head>
+<body>
+
+  <div class="cv-container">
+<?php include("include/two_first_columns.php") ?>
+    <div class="right-column-two">
+      <div class="header">
+      </div>
+      <div class="content">
+        <div class="geekItem body">
+<pre><code>
+&#60;!-- <span class="jobTitle">La plupart des structures des sites 
+    (hors Wordpress) favorisent le référencement 
+    naturel pour faciliter le top ranking. </span>-->
+
+</code></pre>
+<blockquote class="geekItem blockquote">
+  Assur & mf
+</blockquote>
+<pre class="codeNumbers"><code>   4
+   5
+   6
+
+   7
+   8
+</code></pre>
+<pre><code>
+      Réintégration de <span class="vocabularyLevel3">textes statiques</span>;
+      <span class="vocabularyLevel2">Customization</span> de <span class="vocabularyLevel1">gabarits</span> de sites partenaires
+      pour un site comparateur d'assurance;
+      <span class="setBookmark bookmarkLevel1"></span>Personnalisation du <span class="vocabularyLevel1">template mailing</span>;
+      <span class="setBookmark bookmarkLevel3"></span>Intégration d'<span class="vocabularyLevel3">éditeur de texte</span> <span class="vocabularyLevel1">WYSIWYG</span> comme 
+      <span class="vocabularyLevel1">tinyMCE</span>;
+
+</code></pre>
+<blockquote class="geekItem blockquote">
+  William Theviot
+</blockquote>
+
+<pre class="codeNumbers"><code>  14
+  15
+  16
+  17
+  18
+</code></pre>
+<pre><code>
+      Revue et amélioration des fichiers <span class="vocabularyLevel1">CSS</span> / <span class="vocabularyLevel1">HTML</span>;
+      Amélioration de l'aspect visuel du site;
+      Idem l. 8 <span class="callBookmark bookmarkLevel3"></span>
+
+</code></pre>
+<blockquote class="geekItem blockquote">
+  Odilezen
+</blockquote>
+
+<pre class="codeNumbers"><code>  24
+  25
+
+    
+  26
+  27
+</code></pre>
+<pre><code>
+      <span class="vocabularyLevel1">Customization</span> de la structure d'un <span class="vocabularyLevel1">thème bootstrap</span>
+      en fonction d'images et de textes (fournis ou 
+      <span class="vocabularyLevel1">dynamiques</span>);
+      Idem l. 7 <span class="callBookmark bookmarkLevel1"></span>
+
+</code></pre>
+<blockquote class="geekItem blockquote">
+  Ecostreet
+</blockquote>
+
+<pre class="codeNumbers"><code>  33
+  34
+  35
+  36 
+</code></pre>
+<pre><code>
+      <span class="vocabularyLevel2">Wordpress</span> : Rédaction des articles;
+      Remplissage des <span class="vocabularyLevel1">métadonnées</span> pour chaque élément;
+
+</code></pre>
+<blockquote class="geekItem blockquote">
+  Computer Mobile Service
+</blockquote>
+
+<pre class="codeNumbers"><code>  42
+  43
+  
+  44
+</code></pre>
+<pre><code>
+      Création d'un site <span class="vocabularyLevel3">vitrine</span> <span class="vocabularyLevel1">responsive</span> calqué sur
+      une <span class="vocabularyLevel3">maquette</span> tierce fournie par le client;
+
+</code></pre>
+<blockquote class="geekItem blockquote">
+  Universoft
+</blockquote>
+<pre class="codeNumbers"><code>  50
+  51
+  
+  
+  52
+</code></pre>
+<pre><code>
+      Utilisation de la <span class="vocabularyLevel3">bibliothèque</span> <span class="vocabularyLevel1">CSS</span> de <span class="vocabularyLevel1">Qt</span> pour un 
+      aspect visuel claqué sur une application tierse
+      obsolète;
+
+</code></pre>
+<blockquote class="geekItem blockquote">
+  48h de Gujan
+</blockquote>
+<pre class="codeNumbers"><code>  58
+  59
+  
+  60
+</code></pre>
+<pre><code>
+      <span class="vocabularyLevel2">Wordpress</span> : Codage d'un <span class="vocabularyLevel3">thème</span> <span class="vocabularyLevel1">from scratch</span> avec 
+      <span class="vocabularyLevel3">maquette</span> fournie par un <span class="jobTitle">infographiste</span>;
+
+</code></pre>
+          <div class="geekItem logo1">
+  <?php include("logo_html5_for_html.svg"); ?>
+          </div>
+           <div class="geekItem logo2">
+  <?php include("logo_bs_big_for_html.svg"); ?>
+           </div>
+          <div class="geekItem logo3">
+  <?php include("logo_css3_for_html.svg"); ?>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
---
+++ js.php
@@ -0,0 +1,137 @@
+<!-- continuer ici : 
+
+réorganiser, i.e. ajouter des dossiers css, recup_cdn
+
+les versionner avec git
+
+envoyer cvs à karyn
+les mettre sur pole emploi
+voir si on peut les mettre sur linkedin
+
+-->
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Curriculum vitae JS</title>
+  <?php require("include/head.html") ?>
+  <link rel="stylesheet" type="text/css" href="style_js.css">
+</head>
+<body>
+
+  <div class="cv-container">
+<?php include("include/two_first_columns.php") ?>
+    <div class="right-column-two">
+      <div class="header">
+      </div>
+      <div class="content">
+        <div class="geekItem body">
+<pre><code>
+&#60;!-- <span class="jobTitle">Utilisation de JS dans la console des différents
+<span>t</span>navigateurs pour tester la sécurité dans la 
+production et avancer en local. 
+<u>Green Code</u> : utilisation du concept Ajax directement
+avec XHR quand c'est possible.</span> -->
+
+</code></pre>
+          <blockquote class="geekItem blockquote">
+            Assur & mf
+          </blockquote>
+<pre class="codeNumbers"><code>   4
+   5
+
+   6
+   
+
+   8
+   9
+
+  10
+</code></pre>
+ <pre><code>
+      <span class="setBookmark bookmarkLevel3"></span>Récupération et personnalisation de <span class="vocabularyLevel2">scripts</span> tiers
+      et fournis avec le <span class="vocabularyLevel2">package</span> du thème;
+      <span class="setBookmark bookmarkLevel4"></span>Création et archivage de <span class="vocabularyLevel2">scripts</span> maison proposés
+      au client dans le respect des animations des 
+      thèmes embarquées ou fournies par la <span class="vocabularyLevel2">charte</span>;
+      <span class="setBookmark bookmarkLevel1"></span>Utilisation d'<span class="vocabularyLevel2">objets</span> <span class="vocabularyLevel1">JSON</span>;
+      <span class="setBookmark bookmarkLevel2"></span><span class="vocabularyLevel1">Bootstrap</span> : choix d'une version de <span class="vocabularyLevel1">JQuery</span> récente
+      compatible avec la version du <span class="vocabularyLevel2">package</span>;
+
+</code></pre>
+
+      <blockquote class="geekItem blockquote">
+        William Theviot
+      </blockquote>
+
+      <pre class="codeNumbers"><code>  16
+  17
+  18
+  19
+  20
+</code></pre>
+<pre><code>
+      Revue et amélioration des fichiers <span class="vocabularyLevel1">CSS</span> / <span class="vocabularyLevel1">HTML</span>;
+      Amélioration de l'aspect visuel du site;
+      Idem l. 8 <span class="callBookmark bookmarkLevel3"></span>
+    </code></pre>
+
+    <blockquote class="geekItem blockquote">
+      Odilezen
+    </blockquote>
+
+    <pre class="codeNumbers"><code>  26
+  27
+  28
+  29
+  30
+  31
+</code></pre>
+  <pre><code>
+      Idem l. 6 <span class="callBookmark bookmarkLevel4"></span>
+      Idem l. 5 <span class="callBookmark bookmarkLevel3"></span>
+      Idem l. 9 <span class="callBookmark bookmarkLevel2"></span>
+      Idem l. 8 <span class="callBookmark bookmarkLevel1"></span>
+  </code></pre>
+
+<blockquote class="geekItem blockquote">
+  Hôtel les pins
+</blockquote>
+
+<pre class="codeNumbers"><code>  37
+  38
+
+  39
+  40 
+  41
+</code></pre>
+    <pre><code>
+      <span class="vocabularyLevel1">JS Hack</span> pour ajustement graphique selon l'appareil
+      et le navigateur utilisés;
+      Idem l. 5 <span class="callBookmark bookmarkLevel3"></span>
+      Idem l. 8 <span class="callBookmark bookmarkLevel1"></span>
+    </code></pre>
+
+
+<blockquote class="geekItem blockquote">
+  Winamp Podcast Pack
+</blockquote>
+
+<pre class="codeNumbers"><code>  47
+  48
+  
+  
+
+  49
+</code></pre>
+    <pre><code>
+      Utilisation d'un <span class="vocabularyLevel2">script</span> appelant <span class="vocabularyLevel1">JQuery</span> pour
+      générer automatiquement un <span class="vocabularyLevel2">podcast</span> du style
+      du <span class="vocabularyLevel2">plugin</span> disparu <span class="vocabularyLevel2">flash</span>, avec <span class="jobTitle">C++</span> et l'aide d'une 
+      <span class="vocabularyLevel3">bibliothèque</span> de <span class="jobTitle">Qt</span>;
+    </code></pre>
+        <div class="geekItem logo">  <?php include("logo_js_for_html.svg"); ?></div>
+      </div>
+    </div>
+  </div>
+</div>
+</body>
